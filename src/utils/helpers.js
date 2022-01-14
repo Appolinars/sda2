c84dc8ca6645cb63c7463271622f5715e99cf250
appolinaris <vakulenko.maksim977@gmail.com>
2022-01-14 17:02:06 +0200

    remove if

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,9 +1,7 @@
 export const encodeFormData = (data) => {
   const FormData = new FormData();
   Object.keys(data).forEach((value) => {
-    if (value) {
-      FormData.append(value, data[value]);
-    }
+    FormData.append(value, data[value]);
   });
   return FormData;
 };
